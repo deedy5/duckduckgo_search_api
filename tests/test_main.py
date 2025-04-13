b49435a9839dff9a1c341adf0e72854098dedd71
deedy5 <65482418+deedy5@users.noreply.github.com>
2025-04-13 08:50:20 +0300

    tests: fix

--- tests/test_main.py
+++ tests/test_main.py
@@ -4,28 +4,28 @@ from litestar.testing import TestClient
 from duckduckgo_search_api.main import app
 
 
-def test_text():
+def test_text() -> None:
     with TestClient(app=app) as client:
         response = client.get("/text?q=usa")
         assert response.status_code == HTTP_200_OK
-        assert len(response.json()) > 10
+        assert len(response.json()) > 5
 
 
-def test_images():
+def test_images() -> None:
     with TestClient(app=app) as client:
         response = client.get("/images?q=usa")
         assert response.status_code == HTTP_200_OK
         assert len(response.json()) > 10
 
 
-def test_videos():
+def test_videos() -> None:
     with TestClient(app=app) as client:
         response = client.get("/videos?q=usa")
         assert response.status_code == HTTP_200_OK
         assert len(response.json()) > 10
 
 
-def test_news():
+def test_news() -> None:
     with TestClient(app=app) as client:
         response = client.get("/news?q=usa")
         assert response.status_code == HTTP_200_OK
